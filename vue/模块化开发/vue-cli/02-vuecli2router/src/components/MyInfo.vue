94db8be0a5ad80a31d514701b59757f8880c0e3f
duQingShan <duqings@foxmail.com>
2020-08-15 01:50:45 +0800

    nothing

---
+++ vue/模块化开发/vue-cli/02-vuecli2router/src/components/MyInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>duQingShan</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: aquamarine
+  }
+</style>
